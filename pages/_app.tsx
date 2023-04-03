a5205f0f5e5c5cf9f8ddd1d4e6372dbc7ceb1e7f
Simba <simba@getatme.com>
2023-04-03 18:14:07 +0200

    WIP

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,35 @@ import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import Script from 'next/script';
+
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-      <Analytics />
-    </SessionProvider>
+    <>
+
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+
+        {/* <Analytics /> */}
+
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+        <Script strategy="lazyOnload">
+          {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+        </Script>
+        
+
+      </SessionProvider>
+    </>
+
   );
 }
 
